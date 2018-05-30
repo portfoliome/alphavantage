db66d08f125eb70c058d17d1dcb7d7b30a32d81d
pmart123 <philip.martin2007@gmail.com>
2018-05-30 19:03:16 -0400

    Specification for price history API retrieval * Add support for daily price and daily adjusted prices * Add support for intraday prices * Transform response data to be sorted in ascending order * Transform response data fields to include datetime and without record numbers * Provide functionality to convert intraday series into UTC time.

---
+++ alphavantage/price_history.py
@@ -0,0 +1,254 @@
+"""
+Alpha Vantage API Adapter.
+
+API Documentation
+-----------------
+https://www.alphavantage.co/documentation/
+"""
+
+import os
+from operator import itemgetter
+
+import requests
+from foil.formatters import format_repr_info
+from foil.records import rename_keys
+
+from alphavantage.dates import parse_date, parse_datetime, convert_to_utc
+from alphavantage.reference import (
+    INTRADAY, DAILY, WEEKLY, MONTHLY, OPEN, HIGH, LOW, CLOSE,
+    VOLUME, ADJUSTED_CLOSE, DIVIDEND, SPLIT_COEFFICIENT
+)
+from alphavantage import web
+
+
+# output size options
+COMPACT = 'compact'
+FULL = 'full'
+
+# Period Options
+PERIODS = {
+    INTRADAY: 'INTRADAY',
+    DAILY: 'DAILY',
+    WEEKLY: 'WEEKLY',
+    MONTHLY: 'MONTHLY',
+}
+
+API_KEY = os.environ.get('ALPHA_VANTAGE_API_KEY', '')
+
+
+def format_interval(interval):
+    """Format intraday interval."""
+
+    return f'{interval}min'
+
+
+RESPONSE_KEY_MAP = {
+    DAILY: 'Time Series (Daily)',
+    WEEKLY: 'Weekly Time Series',
+    MONTHLY: 'Monthly Time Series'
+}
+
+ADJUSTED_RESPONSE_KEY_MAP = {
+    DAILY: 'Time Series (Daily)',
+    WEEKLY: 'Weekly Adjusted Time Series',
+    MONTHLY: 'Monthly Adjusted Time Series'
+}
+
+INTRADAY_RESPONSE_KEY_MAP = {
+    i: 'Time Series ({})'.format(format_interval(i))
+    for i in [1, 5, ]
+}
+
+FIELD_PARSERS = {
+    OPEN: float,
+    HIGH: float,
+    LOW: float,
+    CLOSE: float,
+    VOLUME: int,
+    ADJUSTED_CLOSE: float,
+    DIVIDEND: float,
+    SPLIT_COEFFICIENT: float
+}
+
+
+class Results:
+    """Container for price history results."""
+
+    def __init__(self, ticker, records, timezone, updated_at, retrieved_at):
+        self.ticker = ticker
+        self.records = records
+        self.timezone = timezone
+        self.updated_at = updated_at
+        self.retrieved_at = retrieved_at
+
+    def __eq__(self, other):
+        return self.ticker == other.ticker
+
+    def __repr__(self):
+        return format_repr_info(['ticker'])
+
+
+class PriceHistory:
+    """Price information summary for list of securities."""
+
+    RESPONSE_MAP = RESPONSE_KEY_MAP
+
+    time_field = 'as_of_date'
+    FIELDS = (OPEN, HIGH, LOW, CLOSE, VOLUME)
+
+    def __init__(self, period=DAILY, output_size=COMPACT, api_key=API_KEY):
+        self.period = period
+        self.output_size = output_size
+        self.api_key = api_key
+        self.field_map = dict(
+            zip(build_field_names(self.FIELDS), self.FIELDS)
+        )
+        self.sort_key = itemgetter(self.time_field)
+        self.session = requests.session()
+
+    def request_parameters(self, ticker):
+        ts_function = get_time_series_function(self.period)
+
+        parameters = {
+            'function': ts_function,
+            'symbol': ticker,
+            'apikey': self.api_key,
+            'outputsize': self.output_size,
+        }
+
+        return parameters
+
+    def get(self, ticker):
+        parameters = self.request_parameters(ticker)
+        response, retrieved_at = web.get(self.session, parameters)
+
+        return self.get_results(ticker, response, retrieved_at)
+
+    def get_results(self, ticker, response, retrieved_at):
+        updated_at, timezone = self.transform_meta_data(response['Meta Data'])
+        records = self.transform_records(response[self.data_key])
+        records = self.sort_records(self.convert_timezones(records, timezone))
+
+        return Results(ticker, records, timezone,
+                       updated_at=updated_at, retrieved_at=retrieved_at)
+
+    def sort_records(self, records):
+        """Sort records in ascending time order."""
+
+        return sorted(list(records), key=self.sort_key)
+
+    def transform_records(self, records):
+        """Convert record field names, parse values and add time field."""
+
+        for time_string, record in records.items():
+            data = self.transform_record(record)
+            data[self.time_field] = self.parse_time(time_string)
+
+            yield data
+
+    @property
+    def data_key(self):
+        return self.RESPONSE_MAP[self.period]
+
+    def parse_time(self, d):
+        return parse_date(d)
+
+    def convert_timezones(self, records, timezone):
+        for record in records:
+            yield record
+
+    def transform_record(self, record):
+        return self.parse_record(self.adapt_data(record))
+
+    def adapt_data(self, record):
+        """Change data record fields."""
+
+        return rename_keys(record, self.field_map)
+
+    def parse_record(self, record):
+        """Parse record field values."""
+
+        return {k: FIELD_PARSERS[k](v) for k, v in record.items()}
+
+    def transform_meta_data(self, response_meta):
+        refresh_key = next(
+            key for key in response_meta if key.endswith('Last Refreshed')
+        )
+        tz_key = next(
+            key for key in response_meta if key.endswith('Time Zone')
+        )
+        updated_at = self.parse_time(response_meta[refresh_key])
+        timezone = response_meta[tz_key]
+
+        return updated_at, timezone
+
+
+class AdjustedPriceHistory(PriceHistory):
+    """Adjusted price history."""
+
+    RESPONSE_MAP = ADJUSTED_RESPONSE_KEY_MAP
+    FIELDS = (
+        OPEN, HIGH, LOW, CLOSE, ADJUSTED_CLOSE, VOLUME,
+        DIVIDEND, SPLIT_COEFFICIENT
+    )
+
+
+class IntradayPriceHistory(PriceHistory):
+    """Intraday price history."""
+
+    time_field = 'as_of_time'
+    RESPONSE_MAP = INTRADAY_RESPONSE_KEY_MAP
+
+    def __init__(self, output_size=COMPACT, api_key=API_KEY, interval=1,
+                 utc=True):
+        super().__init__(
+            period=INTRADAY, output_size=output_size, api_key=api_key
+        )
+        self.interval = interval
+        self.utc = utc
+
+    def request_parameters(self, ticker):
+        parameters = super().request_parameters(ticker)
+        parameters['interval'] = format_interval(self.interval)
+
+        return parameters
+
+    @property
+    def data_key(self):
+        return self.RESPONSE_MAP[self.interval]
+
+    def parse_time(self, d):
+        return parse_datetime(d)
+
+    def convert_timezones(self, records, timezone):
+        if self.utc:
+            for record in records:
+                dt = convert_to_utc(record[self.time_field], timezone)
+                record[self.time_field] = dt
+
+                yield record
+
+    def transform_meta_data(self, response_meta):
+        updated_at, timezone = super().transform_meta_data(response_meta)
+
+        if self.utc:
+            updated_at = convert_to_utc(updated_at, timezone)
+
+        return updated_at, timezone
+
+
+def get_time_series_function(period):
+    """Get the time-series function string"""
+
+    period_code = PERIODS[period]
+
+    return f'TIME_SERIES_{period_code}'
+
+
+def build_field_names(fields):
+    """Build response field names from sorted field list."""
+
+    for index, field in enumerate(fields, 1):
+        field = ' '.join(field.split('_'))
+
+        yield f'{index}. {field.lower()}'
---
+++ tests/test_price_history.py
@@ -0,0 +1,229 @@
+import unittest
+from datetime import date, datetime
+
+import pytz
+
+from alphavantage.dates import convert_to_utc
+from alphavantage.price_history import (
+    AdjustedPriceHistory, build_field_names, get_time_series_function, FULL,
+    IntradayPriceHistory, PriceHistory, format_interval, Results
+)
+from tests.fixtures import (
+    MOCK_META, MOCK_DAILY_RECORDS, MOCK_DAILY_PRICE_RESPONSE,
+    MOCK_ADJUSTED_RECORDS, MOCK_INTRADAY_RESPONSE
+)
+
+
+class TestHelperFunctions(unittest.TestCase):
+    def test_build_field_names(self):
+        fields = ['alpha', 'beta_gamma']
+
+        expected = ['1. alpha', '2. beta gamma']
+        result = list(build_field_names(fields))
+
+        self.assertEqual(expected, result)
+
+    def test_convert_to_utc_winter(self):
+        d = datetime(2018, 1, 25, 16, 0, 0)
+
+        expected = datetime(2018, 1, 25, 21, 0, 0, tzinfo=pytz.UTC)
+        result = convert_to_utc(d, 'US/Eastern')
+
+        self.assertEqual(expected, result)
+
+    def test_convert_to_utc(self):
+        d = datetime(2018, 5, 25, 16, 0, 0)
+
+        expected = datetime(2018, 5, 25, 20, 0, 0, tzinfo=pytz.UTC)
+        result = convert_to_utc(d, 'US/Eastern')
+
+        self.assertEqual(expected, result)
+
+    def test_get_time_series_function(self):
+        expected = 'TIME_SERIES_MONTHLY'
+        result = get_time_series_function('M')
+
+        self.assertEqual(expected, result)
+
+    def test_format_interval(self):
+        expected = '5min'
+        result = format_interval(5)
+
+        self.assertEqual(expected, result)
+
+
+class TestPriceHistory(unittest.TestCase):
+    def setUp(self):
+        self.ticker = 'MSFT'
+        self.timezone = 'US/Eastern'
+        self.updated_at = date(2018, 5, 25)
+        self.api_key = 'my_fake_key'
+        self.price_history = PriceHistory(api_key=self.api_key, output_size=FULL)
+        self.transformed_records = [
+            {
+                'as_of_date': date(2018, 5, 25),
+                'open': 98.3000,
+                'high': 98.9800,
+                'low': 97.8600,
+                'close': 98.3600,
+                'volume': 18363918
+            },
+            {
+                'as_of_date': date(2018, 5, 24),
+                'open': 98.7250,
+                'high': 98.9400,
+                'low': 96.8100,
+                'close': 98.3100,
+                'volume': 26649287
+            }
+        ]
+
+    def test_request_parameters(self):
+        expected = {
+            'function': 'TIME_SERIES_DAILY',
+            'symbol': self.ticker,
+            'apikey': self.api_key,
+            'outputsize': FULL
+        }
+        result = self.price_history.request_parameters(self.ticker)
+
+        self.assertEqual(expected, result)
+
+    def test_transform_records(self):
+        expected = self.transformed_records
+        result = list(self.price_history.transform_records(MOCK_DAILY_RECORDS))
+
+        self.assertEqual(expected, result)
+
+    def test_sort_records(self):
+        expected = self.transformed_records[::-1]
+        result = self.price_history.sort_records(self.transformed_records)
+
+        self.assertEqual(expected, result)
+
+    def test_transform_meta_data(self):
+        expected = (self.updated_at, self.timezone)
+        result = self.price_history.transform_meta_data(MOCK_META)
+
+        self.assertEqual(expected, result)
+
+    def test_get_results(self):
+        retrieved_at = datetime(2018, 5, 25, 9, 0, 30)
+        expected = Results(ticker=self.ticker,
+                           records=self.transformed_records[::-1],
+                           retrieved_at=retrieved_at,
+                           updated_at=self.updated_at,
+                           timezone=self.timezone)
+        result = self.price_history.get_results(
+            self.ticker, MOCK_DAILY_PRICE_RESPONSE, retrieved_at
+        )
+
+        self.assertEqual(expected.retrieved_at, result.retrieved_at)
+        self.assertEqual(expected.updated_at, result.updated_at)
+        self.assertEqual(expected.records, result.records)
+
+
+class TestAdjustedPriceHistory(unittest.TestCase):
+    def setUp(self):
+        self.price_history = AdjustedPriceHistory()
+        self.transformed_records = [
+            {
+                'as_of_date': date(2018, 5, 25),
+                'open': 98.3000,
+                'high': 98.9800,
+                'low': 97.8600,
+                'close': 98.3600,
+                'adjusted_close': 98.3600,
+                'volume': 17942632,
+                'dividend_amount': 0,
+                'split_coefficient': 1.0
+            },
+            {
+                'as_of_date': date(2018, 5, 24),
+                'open': 98.7250,
+                'high': 98.9400,
+                'low': 96.8100,
+                'close': 98.31000,
+                'adjusted_close': 98.3100,
+                'volume': 26649287,
+                'dividend_amount': 0,
+                'split_coefficient': 1.0
+            },
+            {
+                'as_of_date': date(2018, 5, 23),
+                'open': 96.7100,
+                'high': 98.7300,
+                'low': 96.3200,
+                'close': 98.6600,
+                'adjusted_close': 98.6600,
+                'volume': 21251222,
+                'dividend_amount': 0,
+                'split_coefficient': 1.0
+            }
+        ]
+
+    def test_transform_records(self):
+        expected = self.transformed_records
+        result = list(self.price_history.transform_records(MOCK_ADJUSTED_RECORDS))
+
+        self.assertEqual(expected, result)
+
+
+class TestIntraDataPriceHistory(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+        self.ticker = 'MSFT'
+        self.timezone = 'US/Eastern'
+        self.api_key = 'my_fake_key'
+        self.interval = 1
+        self.price_history = IntradayPriceHistory(
+            api_key=self.api_key, output_size=FULL, interval=self.interval
+        )
+        self.transformed_records = [
+            {'as_of_time': datetime(2018, 5, 30, 20, 0, 0, tzinfo=pytz.UTC),
+             'open': 99.0000,
+             'high': 99.0500,
+             'low': 98.9100,
+             'close': 98.9500,
+             'volume': 2233252},
+            {'as_of_time': datetime(2018, 5, 30, 19, 59, 0, tzinfo=pytz.UTC),
+             'open': 99.0350,
+             'high': 99.0500,
+             'low': 99.0000,
+             'close': 99.0000,
+             'volume': 156349},
+            {'as_of_time': datetime(2018, 5, 30, 19, 58, 0, tzinfo=pytz.UTC),
+             'open': 98.9900,
+             'high': 99.0600,
+             'low': 98.9900,
+             'close': 99.0300,
+             'volume': 142621}
+        ]
+
+    def test_request_parameters(self):
+        expected = {
+            'function': 'TIME_SERIES_INTRADAY',
+            'symbol': self.ticker,
+            'apikey': self.api_key,
+            'outputsize': FULL,
+            'interval': f'{self.interval}min'
+        }
+        result = self.price_history.request_parameters(self.ticker)
+
+        self.assertEqual(expected, result)
+
+    def test_get_results(self):
+        retrieved_at = datetime(2018, 5, 30, 9, 0, 30)
+        updated_at = datetime(2018, 5, 30, 20, 0, 0, tzinfo=pytz.UTC)
+        expected = Results(ticker=self.ticker,
+                           records=self.transformed_records[::-1],
+                           retrieved_at=retrieved_at,
+                           updated_at=updated_at,
+                           timezone=self.timezone)
+        result = self.price_history.get_results(
+            self.ticker, MOCK_INTRADAY_RESPONSE, retrieved_at
+        )
+
+        self.assertEqual(expected.retrieved_at, result.retrieved_at)
+        self.assertEqual(expected.updated_at, result.updated_at)
+        self.assertEqual(expected.records, result.records)

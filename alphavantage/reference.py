7e845c3f9335eb3ed106f23dce4fcf838455c400
pmart123 <philip.martin2007@gmail.com>
2018-05-30 18:59:56 -0400

    Reference file for field names, base url, interval periods.

---
+++ alphavantage/reference.py
@@ -0,0 +1,17 @@
+BASE_URL = 'http://www.alphavantage.co/query'
+
+# time period flags
+INTRADAY = 'I'
+DAILY = 'D'
+WEEKLY = 'W'
+MONTHLY = 'M'
+
+# field names
+OPEN = 'open'
+HIGH = 'high'
+LOW = 'low'
+CLOSE = 'close'
+VOLUME = 'volume'
+ADJUSTED_CLOSE = 'adjusted_close'
+DIVIDEND = 'dividend_amount'
+SPLIT_COEFFICIENT = 'split_coefficient'
